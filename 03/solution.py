50abe421c8ef78c2b82ff880e121bb254c7a2b9c
dimitris <dimitris.giatsios@gmail.com>
2025-12-14 01:30:50 +0200

    Making is_valid function more explicit in day 3.

--- 03/solution.py
+++ 03/solution.py
@@ -7,37 +7,39 @@ lengths for each candidate triangle are read three at a time
 column-wise, with no correlation between columns.
 '''
 
+
 def is_valid(lengths):
     '''Given an iterable of 3 positive numeric values, return True
     if they satisfy triangle inequality, else return False'''
-    return max(lengths) < sum(lengths) - max(lengths)
-
-def line_to_tuple(line):
-    return tuple(map(int,line.strip().split()))
+    a, b, c = sorted(lengths)
+    return a + b > c
 
 
-# ------------------- Part 1 --------------------- #
+def parse_triangle(line):
+    return tuple(map(int,line.split()))
 
-with open('input.txt') as data:
-    valid_count = sum(map(is_valid, map(line_to_tuple, data)))
 
-print(valid_count)
+if __name__ == '__main__':
 
-# ------------------- Part 2 --------------------- #
+    # ------------------- Part 1 --------------------- #
+    with open('input.txt') as data:
+        valid_count = sum(map(is_valid, map(parse_triangle, data)))
 
-valid_count = 0
+    print(valid_count)
 
-with open('input.txt') as data:
-    '''
-    [data]*3 gives 3 pointers to the file iterator (cf. grouper recipe).
-    Iterating over the zip of this, after mapping lines to tuples,
-    gives us a 3-by-3 batch of data at a time, as a tuple of three 
-    tuples, for example: ( (5,6,3), (2,4,5), (1,5,2) ). 
-    We zip each of those batches to read it column-wise, turning the
-    above to: ( (5,2,1), (6,4,5), (3,5,2) ), and check how many of those
-    are valid triangles, to update the aggregate valid count.
-    '''
-    for batch in zip(*[map(line_to_tuple, data)]*3):
-        valid_count += sum(map(is_valid, zip(*batch)))
-       
-print(valid_count)
+    # ------------------- Part 2 --------------------- #
+    valid_count = 0
+
+    with open('input.txt') as data:
+        '''
+        [data]*3 gives 3 pointers to the file iterator (cf. grouper recipe).
+        Iterating over the zip of this, after mapping lines to tuples,
+        gives us a 3-by-3 batch of data at a time, as a tuple of three 
+        tuples, for example: ( (5,6,3), (2,4,5), (1,5,2) ). 
+        We zip each of those batches to read it column-wise, turning the
+        above to: ( (5,2,1), (6,4,5), (3,5,2) ).
+        '''
+        for batch in zip(*[map(parse_triangle, data)]*3):
+            valid_count += sum(map(is_valid, zip(*batch)))
+        
+    print(valid_count)
