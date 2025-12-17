b8fc9e552585905a882be3a9e8c0b534361b6cc3
dimitris <dimitris.giatsios@gmail.com>
2025-12-18 01:26:26 +0200

    Minor enhancement for day 8.
    - Replaced eval with a constant dict mapping strings to function names.
    - screen is now a parameter in the functions, not a global variable.
    - Used type hints and function doc-strings.

--- 08/solution.py
+++ 08/solution.py
@@ -15,47 +15,69 @@ instructions, where each letter is 5 pixels wide and 6 tall.
 '''
 
 import numpy as np
+import numpy.typing as npt
+import matplotlib.pyplot as plt
 import re
 
-screen = np.zeros([6,50], dtype=bool)
-
-def rect(y, x):
+def rect(screen: npt.NDArray[np.bool_], y: int, x: int) -> None:
+    '''Given a boolean 2D array, set its top left x-by-y items to True'''
+    if screen.ndim != 2:
+        raise ValueError("screen must be 2-dimensional")
     # Arguments are "wide" and "tall", so reversed to
     # correspond to rows and columns in array
     screen[0:x,0:y] = True
 
-def rotate_row(row, shift):
+
+def rotate_row(screen: np.ndarray, row: int, shift: int) -> None:
+    '''Shift items in a row of a 2D array right by a number of positions'''
+    if screen.ndim != 2:
+        raise ValueError("screen must be 2-dimensional")
     screen[row, :] = np.roll(screen[row, :], shift)
 
-def rotate_column(column, shift):
+
+def rotate_column(screen: np.ndarray, column: int, shift: int) -> None:
+    '''Shift items in a column of a 2D array down by a number of positions'''
+    if screen.ndim != 2:
+        raise ValueError("screen must be 2-dimensional")
     screen[:,column] = np.roll(screen[:,column], shift)
 
-def follow_instruction(instruction):
+
+# Map instruction strings to functions; must come after function definitions
+ACTIONS = {
+    'rect': rect,
+    'rotate row': rotate_row,
+    'rotate column': rotate_column
+}
+
+def follow_instruction(screen: np.ndarray, instruction: str) -> None:
     '''Read a single instruction string and configure pixels appropriately'''
     action = re.match(r'(rect|rotate row|rotate column)', instruction).group()
     args = map(int, re.findall(r'\d+', instruction))
-    eval(action.replace(' ','_'))(*args)
+    ACTIONS[action](screen, *args)
 
-with open('input.txt') as f:
-    for instruction in f: 
-        follow_instruction(instruction)
-        
-print(f"Part 1: Number of lit pixels: {screen.sum()}.")
-print('-'*40)
 
-# ------- Part 2 ------ #
+if __name__ == '__main__':
 
-print('Part 2: Screen view after instructions:')
-print()
+    screen = np.zeros([6,50], dtype=bool)
 
-# View screen without matplotlib
-translate = {True:'.', False:' '}
-for row in screen:
-    for elem in row:
-        print(translate[elem], end='')
+    with open('input.txt') as f:
+        for instruction in f: 
+            follow_instruction(screen, instruction)
+
+    # ------- Part 1 ------ #
+    print(f"Part 1: Number of lit pixels: {screen.sum()}.")
+    print('-'*40)
+
+    # ------- Part 2 ------ #
+    print('Part 2: Screen view after instructions:')
     print()
 
-# View screen using matplotlib
-from matplotlib import pyplot as plt
-im = plt.imshow(screen, cmap="copper_r")
+    # View screen without matplotlib
+    translate = {True:'.', False:' '}
+    for row in screen:
+        print(''.join('#' if pixel else ' ' for pixel in row))
 
+    # View screen using matplotlib
+    plt.imshow(screen, cmap="copper_r")
+    plt.axis("off")
+    plt.show()
