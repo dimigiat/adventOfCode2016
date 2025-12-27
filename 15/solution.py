4a60ad979b687ac22768b21f09035ec23830fbec
dimitris <dimitris.giatsios@gmail.com>
2025-12-27 22:21:12 +0200

    Slight refactoring of day 15 solution. - For loop in main function made simpler. - Offset replaced with (disc + 1) in loop - Type hints - time.perf_counter() for benchamarking

--- 15/solution.py
+++ 15/solution.py
@@ -15,36 +15,60 @@ want to try a brute force solution, like we do here.
 (https://brilliant.org/wiki/chinese-remainder-theorem/)
 '''
 
-def parseLine(line):
-    return [int(i) for i in line.strip(".\n").split() if i.isdigit()]
+from time import perf_counter
 
-with open("input.txt") as f:
-    numPositions, initials = map(list, zip(*map(parseLine, f)))
 
-def discAtZero(offset, initial, numPositions, t):
-    return (t + offset + initial) % numPositions == 0
+def parsed_line(line: str) -> list[int]:
+    return [int(i) for i in line.strip('.\n').split() if i.isdigit()]
 
-def getPushTime(initials, numPositions):
-    numDiscs = len(initials)
-    offsets = list(range(1, numDiscs + 1))
-    pushTime = 0
+
+def is_aligned(t: int, offset: int, initial: int, num_positions: int) -> bool:
+    return (t + offset + initial) % num_positions == 0
+
+
+def press_time(initials: tuple[int], num_positions: tuple[int]) -> int:
+    '''
+    Given the initial position of each disc, and the total number of its
+    positions, return the number of seconds to wait before pressing the
+    button to drop the capsule, so that the latter reaches all discs at
+    their 0-th position.
+    '''
+    num_discs = len(initials)
+    t = 0
+
+    # Guaranteed to terminate because a solution exists for the given input
     while True:
-        found = True
-        for disc in range(numDiscs):
-            if not discAtZero(offsets[disc], initials[disc],
-                              numPositions[disc], pushTime):
-                found = False
+        for disc in range(num_discs):
+            if not is_aligned(t, disc + 1, initials[disc], num_positions[disc]):
                 break
-        if found:
-            return pushTime
-        pushTime += 1
+        else:
+            return t
+        t += 1
 
-print(f"Part 1: Push button at time: {getPushTime(initials, numPositions)}")
 
-numPositions.append(11)
-initials.append(0)
+if __name__ == '__main__':
 
-print(f"Part 2: Push button at time: {getPushTime(initials, numPositions)}")
+    with open('input.txt') as f:
+        num_positions, initials = map(tuple, zip(*map(parsed_line, f)))
+
+    # ---------------------------- Part 1 --------------------------------- #
+    start = perf_counter()
+    print(f'Part 1: Press at time: {press_time(initials, num_positions)}')
+    stop = perf_counter()
+    elapsed = stop - start
+    print(f'Time elapsed: {elapsed} seconds.')
+
+    # ---------------------------- Part 2 --------------------------------- #
+
+    # One more disc added on top of the others for part 2.
+    num_positions = (11,) + num_positions
+    initials = (0,) + initials
+
+    start = perf_counter()
+    print(f'Part 2: Press at time: {press_time(initials, num_positions)}')
+    stop = perf_counter()
+    elapsed = stop - start
+    print(f'Time elapsed: {elapsed} seconds.')
 
 
 
